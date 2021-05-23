6c05f0e12ed8bd7da2d24211cd33bbb40c48b11f
JorgeBritezG <40007364+JorgeBritezG@users.noreply.github.com>
2021-05-23 15:38:52 -0400

    AlbumEntityConfig

---
+++ ImagePick.DataAccess/ImagePickDbContext.cs
@@ -0,0 +1,34 @@
+﻿using ImagePick.DataAccess.Contracts.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ImagePick.DataAccess
+{
+    public class ImagePickDbContext : DbContext
+    {
+
+        public ImagePickDbContext( DbContextOptions options ) : base(options)
+        {
+
+        }
+
+        public DbSet<Album> Albums { get; set; }
+
+        public DbSet<Image> Images { get; set; }
+
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+
+
+
+            base.OnModelCreating(modelBuilder);
+
+        }
+
+    }
+}
