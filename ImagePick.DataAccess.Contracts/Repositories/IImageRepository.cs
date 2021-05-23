9e19573cb0a36a1fcca2167c1c08a42f670873f1
JorgeBritezG <40007364+JorgeBritezG@users.noreply.github.com>
2021-05-23 14:31:42 -0400

    IImageRepository

---
+++ ImagePick.DataAccess.Contracts/Repositories/IImageRepository.cs
@@ -0,0 +1,19 @@
+﻿using ImagePick.DataAccess.Contracts.Entities;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace ImagePick.DataAccess.Contracts.Repositories
+{
+    public interface IImageRepository
+    {
+        Task<IEnumerable<Image>> GetAllAsync();
+
+        Task<Image> GetAsync( int id );
+
+        Task<Image> DeleteAsync( int id );
+
+        Task<Image> UpdateAsync( Image entity );
+
+        Task<Image> AddAsync( Image entity );
+    }
+}
