92e45e0c2d08e52ab9ce70ae3ba5c50eaef8d006
JorgeBritezG <40007364+JorgeBritezG@users.noreply.github.com>
2021-05-23 14:30:16 -0400

    IAlbumRepository

---
+++ ImagePick.DataAccess.Contracts/Repositories/IAlbumRepository.cs
@@ -0,0 +1,19 @@
+﻿using ImagePick.DataAccess.Contracts.Entities;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace ImagePick.DataAccess.Contracts.Repositories
+{
+    public interface IAlbumRepository
+    {
+        Task<IEnumerable<Album>> GetAllAsync();
+
+        Task<Album> GetAsync( int id );
+
+        Task<Album> DeleteAsync( int id );
+
+        Task<Album> UpdateAsync( Album entity );
+
+        Task<Album> AddAsync( Album entity );
+    }
+}
