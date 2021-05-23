486d9550429ca777d0c3fc12d513c108ac94c556
JorgeBritezG <40007364+JorgeBritezG@users.noreply.github.com>
2021-05-23 17:32:15 -0400

    Image and User EntityConfig

---
+++ ImagePick.DataAccess/EntityConfig/ImageEntityConfig.cs
@@ -0,0 +1,24 @@
+﻿using ImagePick.DataAccess.Contracts.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace ImagePick.DataAccess.EntityConfig
+{
+    public static class ImageEntityConfig
+    {
+        public static void SetEntityBuilder( EntityTypeBuilder<Image> modelBuilder )
+        {
+
+            modelBuilder.HasKey(x => x.Id);
+            modelBuilder.Property(x => x.Id).IsRequired();
+
+            modelBuilder.Property(x => x.UserName).IsRequired();
+            modelBuilder.Property(x => x.UserName).HasMaxLength(100);
+
+            modelBuilder.HasOne(x => x.Album)
+                        .WithMany(x => x.Images)
+                        .OnDelete(DeleteBehavior.Cascade);
+
+        }
+    }
+}
