b79b917553ac9aacc888ae369aefb7c398ec4d48
JorgeBritezG <40007364+JorgeBritezG@users.noreply.github.com>
2021-05-23 14:28:41 -0400

    IUserRepository

---
+++ ImagePick.DataAccess.Contracts/Repositories/IUserRepository.cs
@@ -0,0 +1,19 @@
+﻿using ImagePick.DataAccess.Contracts.Entities;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace ImagePick.DataAccess.Contracts.Repositories
+{
+    public interface IUserRepository
+    {
+        Task<IEnumerable<User>> GetAllAsync();
+
+        Task<User> GetAsync( int id );
+
+        Task<User> DeleteAsync( int id );
+
+        Task<User> UpdateAsync( User entity );
+
+        Task<User> AddAsync( User entity );
+    }
+}
