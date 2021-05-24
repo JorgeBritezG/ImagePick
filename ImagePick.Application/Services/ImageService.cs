8f39a3f11724033e46355e9eed8a7970eda8973b
JorgeBritezG <40007364+JorgeBritezG@users.noreply.github.com>
2021-05-23 20:31:22 -0400

    ImageService

---
+++ ImagePick.Application/Services/ImageService.cs
@@ -0,0 +1,53 @@
+﻿using ImagePick.Application.Contracts.Mappers;
+using ImagePick.Application.Contracts.Models;
+using ImagePick.Application.Contracts.Services;
+using ImagePick.DataAccess.Contracts.Repositories;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ImagePick.Application.Services
+{
+    public class ImageService : IImageService
+    {
+        private readonly IImageRepository _albumRepository;
+
+        public ImageService(IImageRepository albumRepository)
+        {
+            _albumRepository = albumRepository;
+        }
+
+        public async Task<ImageApplication> AddAsync( ImageApplication entity )
+        {
+            var result = await _albumRepository.AddAsync(ImageMapper.Map(entity));
+
+            return ImageMapper.Map(result);
+        }
+
+        public async Task<bool> DeleteAsync( int id )
+        {
+            return await _albumRepository.DeleteAsync(id);
+        }
+
+        public async Task<IEnumerable<ImageApplication>> GetAllAsync()
+        {
+            var result = await _albumRepository.GetAllAsync();
+
+            return result.Select(ImageMapper.Map);
+        }
+
+        public async Task<ImageApplication> GetAsync( int id )
+        {
+            var entity = await _albumRepository.GetAsync(id);
+
+            return ImageMapper.Map(entity);
+        }
+
+        public async Task<ImageApplication> UpdateAsync( ImageApplication entity )
+        {
+            var result = await _albumRepository.UpdateAsync(ImageMapper.Map(entity));
+
+            return ImageMapper.Map(result);
+        }
+    }
+}
