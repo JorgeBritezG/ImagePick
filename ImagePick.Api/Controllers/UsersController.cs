1052b918c16e4d283736d2fb2915211d2b11f98b
JorgeBritezG <40007364+JorgeBritezG@users.noreply.github.com>
2021-05-23 22:06:38 -0400

    UserController Completed

---
+++ ImagePick.Api/Controllers/UsersController.cs
@@ -0,0 +1,140 @@
+﻿using ImagePick.Application.Contracts.Models;
+using ImagePick.Application.Contracts.Services;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace ImagePick.Api.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly IUserService _userService;
+
+        public UsersController(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<UserApplication>>> GetAll()
+        {
+            try
+            {
+                var result = await _userService.GetAllAsync();
+
+                return Ok(result);
+
+            }
+            catch ( Exception ex)
+            {
+
+                return Conflict(ex.Message);
+
+                throw;
+            }
+        }
+
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<UserApplication>> Get(int id)
+        {
+            try
+            {
+                var result = await _userService.GetAsync(id);
+
+                if (result == null)
+                {
+                    return NotFound();
+                }
+
+                return Ok(result);
+
+            }
+            catch ( Exception ex )
+            {
+
+                return Conflict(ex.Message);
+
+                throw;
+            }
+        }
+
+
+        [HttpPost]
+        public async Task<ActionResult<UserApplication>> Post(
+            [FromBody] UserApplication model )
+        {
+            try
+            {
+                if(!ModelState.IsValid) 
+                {
+                    return BadRequest(ModelState);
+                }
+
+                var result = await _userService.AddAsync(model);
+
+                return Created("", result);
+
+            }
+            catch ( Exception ex )
+            {
+
+                return Conflict(ex.Message);
+                throw;
+            }
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<UserApplication>> Put(
+            [FromBody] UserApplication model )
+        {
+            try
+            {
+                if ( !ModelState.IsValid )
+                {
+                    return BadRequest(ModelState);
+                }
+
+                var result = await _userService.UpdateAsync(model);
+
+                return Created("", result);
+
+            }
+            catch ( Exception ex )
+            {
+
+                return Conflict(ex.Message);
+                throw;
+            }
+        }
+
+        public async Task<ActionResult<bool>> Delete( int id )
+        {
+            try
+            {
+
+                var result = await _userService.GetAsync(id);
+
+                if (result == null )
+                {
+                    return NotFound();
+                }
+
+                return Ok(await _userService.DeleteAsync(id));
+
+
+            }
+            catch ( Exception ex )
+            {
+
+                return Conflict(ex.Message);
+                throw;
+            }
+        }
+
+    }
+}
--- ImagePick.Application.Contracts/Models/UserApplication.cs
+++ ImagePick.Application.Contracts/Models/UserApplication.cs
@@ -1,17 +1,25 @@
 ﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace ImagePick.Application.Contracts.Models
 {
     public class UserApplication
     {
+
         public int Id { get; set; }
 
+        [Required]
+        [MaxLength(256)]
         public string Email { get; set; }
 
         public string Name { get; set; }
 
+        [Required]
+        [MaxLength(100)]
         public string FirstName { get; set; }
 
+
+        [MaxLength(100)]
         public string LastName { get; set; }
 
         public virtual ICollection<AlbumApplication> Albums { get; set; }
