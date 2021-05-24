a330c27ddb2f2493006e69766466c54a04c85474
JorgeBritezG <40007364+JorgeBritezG@users.noreply.github.com>
2021-05-23 22:16:45 -0400

    AlbumsController

---
+++ ImagePick.Api/Controllers/AlbumsController.cs
@@ -0,0 +1,140 @@
+﻿using ImagePick.Application.Contracts.Models;
+using ImagePick.Application.Contracts.Services;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace ImagePick.Api.Controllers
+{
+    [Produces("application/json")]
+    [Route("[controller]")]
+    [ApiController]
+    public class AlbumsController : ControllerBase
+    {
+        private readonly IAlbumService _albumService;
+
+        public AlbumsController( IAlbumService albumService )
+        {
+            _albumService = albumService;
+        }
+
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<AlbumApplication>>> GetAll()
+        {
+            try
+            {
+                var result = await _albumService.GetAllAsync();
+
+                return Ok(result);
+
+            }
+            catch ( Exception ex )
+            {
+
+                return Conflict(ex.Message);
+
+                throw;
+            }
+        }
+
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<AlbumApplication>> Get( int id )
+        {
+            try
+            {
+                var result = await _albumService.GetAsync(id);
+
+                if ( result == null )
+                {
+                    return NotFound();
+                }
+
+                return Ok(result);
+
+            }
+            catch ( Exception ex )
+            {
+
+                return Conflict(ex.Message);
+
+                throw;
+            }
+        }
+
+
+        [HttpPost]
+        public async Task<ActionResult<AlbumApplication>> Post(
+            [FromBody] AlbumApplication model )
+        {
+            try
+            {
+                if ( !ModelState.IsValid )
+                {
+                    return BadRequest(ModelState);
+                }
+
+                var result = await _albumService.AddAsync(model);
+
+                return Created("", result);
+
+            }
+            catch ( Exception ex )
+            {
+
+                return Conflict(ex.Message);
+                throw;
+            }
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<AlbumApplication>> Put(
+            [FromBody] AlbumApplication model )
+        {
+            try
+            {
+                if ( !ModelState.IsValid )
+                {
+                    return BadRequest(ModelState);
+                }
+
+                var result = await _albumService.UpdateAsync(model);
+
+                return Created("", result);
+
+            }
+            catch ( Exception ex )
+            {
+
+                return Conflict(ex.Message);
+                throw;
+            }
+        }
+
+        public async Task<ActionResult<bool>> Delete( int id )
+        {
+            try
+            {
+
+                var result = await _albumService.GetAsync(id);
+
+                if ( result == null )
+                {
+                    return NotFound();
+                }
+
+                return Ok(await _albumService.DeleteAsync(id));
+
+
+            }
+            catch ( Exception ex )
+            {
+
+                return Conflict(ex.Message);
+                throw;
+            }
+        }
+    }
+}
