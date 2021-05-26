4cecc7b73a808d96a603ddf3a40943a5951ea851
JorgeBritezG <40007364+JorgeBritezG@users.noreply.github.com>
2021-05-25 21:31:23 -0400

    UserApplicationStub

--- ImagePick.Application.Tests/Services/UserServiceTest.cs
+++ ImagePick.Application.Tests/Services/UserServiceTest.cs
@@ -1,4 +1,10 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using ImagePick.Application.Contracts.Services;
+using ImagePick.Application.Services;
+using ImagePick.Application.Unit.Tests.MockRespository;
+using ImagePick.Application.Unit.Tests.Stubs;
+using ImagePick.DataAccess.Contracts.Repositories;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -9,14 +15,25 @@ namespace ImagePick.Application.Unit.Tests.Services
     [TestClass]
     public class UserServiceTest
     {
+        private static IUserService _userService;
+
+        [ClassInitialize]
+        public static void Setup(TestContext context)
+        {
+            Mock<IUserRepository> _userRepository = new UserRepositoryMock()._userRepository;
+
+            _userService = new UserService(_userRepository.Object);
+        }
 
         [TestMethod]
-        public async Task CreateNewUser()
+        public async Task CreateNewUser_ok()
         {
             //Arrange            
-
+            var expected = UserStub.user_1;
 
             //Act
+
+
 
             //Assert
 
---
+++ ImagePick.Application.Tests/Stubs/UserApplicationStub.cs
@@ -0,0 +1,32 @@
+﻿using ImagePick.Application.Contracts.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ImagePick.Application.Unit.Tests.Stubs
+{
+    public static class UserApplicationStub
+    {
+        public static UserApplication user_null = null;
+
+        public static UserApplication user_1 = new UserApplication()
+        {
+            Id = 1,
+            Email = "email@prueba.com",
+            FirstName = "Jorge",
+            LastName = "Britez",
+        };
+
+        public static UserApplication user_2 = new UserApplication()
+        {
+            Id = 2,
+            Email = "email2@prueba.com",
+            FirstName = "Jorge2",
+            LastName = "Britez2",
+        };
+
+        public static List<UserApplication> users =
+            new List<UserApplication>(new UserApplication[] { user_1, user_2 });
+
+    }
+}
