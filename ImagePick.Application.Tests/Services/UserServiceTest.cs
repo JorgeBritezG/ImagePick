7909eba83c6360607737fcef5f0ca50c5a06de26
JorgeBritezG <40007364+JorgeBritezG@users.noreply.github.com>
2021-05-25 20:31:35 -0400

    install nugets moq and fluentAssertions

---
+++ ImagePick.Application.Tests/Services/UserServiceTest.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImagePick.Application.Unit.Tests.Services
+{
+    [TestClass]
+    public class UserServiceTest
+    {
+
+        [TestMethod]
+        public async Task CreateNewUser()
+        {
+            //Arrange
+            
+
+
+            //Act
+
+            //Assert
+
+        }
+
+
+    }
+}
