29a327bbfd4131c006f5c40d0dce5bbe681bae4e
JorgeBritezG <40007364+JorgeBritezG@users.noreply.github.com>
2021-05-25 21:17:38 -0400

    UserRepositoryMock

--- ImagePick.Application.Tests/Stubs/UserStub.cs
+++ ImagePick.Application.Tests/Stubs/UserStub.cs
@@ -1,12 +1,13 @@
 ﻿using ImagePick.DataAccess.Contracts.Entities;
-using System;
 using System.Collections.Generic;
-using System.Text;
 
 namespace ImagePick.Application.Unit.Tests.Stubs
 {
     public static class UserStub
     {
+        
+        public static User user_null = null;
+
         public static User user_1 = new User()
         {
             Id = 1,
@@ -14,5 +15,17 @@ namespace ImagePick.Application.Unit.Tests.Stubs
             FirstName = "Jorge",
             LastName = "Britez",             
         };
+
+        public static User user_2 = new User()
+        {
+            Id = 2,
+            Email = "email2@prueba.com",
+            FirstName = "Jorge2",
+            LastName = "Britez2",
+        };
+
+        public static List<User> users = 
+            new List<User>(new User[] { user_1, user_2 });
+
     }
 }
