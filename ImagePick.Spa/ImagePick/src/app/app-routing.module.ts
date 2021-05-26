bfb57bbd3dd7499152f8c5c4fb01d6e08a446d17
JorgeBritezG <40007364+JorgeBritezG@users.noreply.github.com>
2021-05-26 19:48:00 -0400

    NavBar component

--- ImagePick.Spa/ImagePick/src/app/app-routing.module.ts
+++ ImagePick.Spa/ImagePick/src/app/app-routing.module.ts
@@ -2,6 +2,20 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
+  // {
+  //   path: '',
+  //   redirectTo: 'home',
+  //   pathMatch: 'full',    
+  // },
+  {
+    path: '',
+    data: {
+      title: 'Home'
+    },
+    loadChildren: () => import('./home-page/home-page.module').then(m => m.HomePageModule)
+    
+  },
+  { path: '**', redirectTo: '' }
   
 ];
 
--- ImagePick.Spa/ImagePick/src/app/home-page/home-page.module.ts
+++ ImagePick.Spa/ImagePick/src/app/home-page/home-page.module.ts
@@ -2,13 +2,18 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { HomePageRoutingModule } from './home-page-routing.module';
+import { HomeComponent } from './home/home.component';
+import { NavbarModule } from '../components/navbar/navbar.module';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    HomeComponent
+  ],
   imports: [
     CommonModule,
-    HomePageRoutingModule
+    HomePageRoutingModule,
+    NavbarModule,
   ]
 })
 export class HomePageModule { }
