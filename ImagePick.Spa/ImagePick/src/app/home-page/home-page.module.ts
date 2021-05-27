30fff30dbaf0feb6f2560d096275a3037d78bfd7
JorgeBritezG <40007364+JorgeBritezG@users.noreply.github.com>
2021-05-26 21:01:09 -0400

    breakpoint before install bootstrap icons

--- ImagePick.Spa/ImagePick/src/app/home-page/home-page.module.ts
+++ ImagePick.Spa/ImagePick/src/app/home-page/home-page.module.ts
@@ -4,6 +4,7 @@ import { CommonModule } from '@angular/common';
 import { HomePageRoutingModule } from './home-page-routing.module';
 import { HomeComponent } from './home/home.component';
 import { NavbarModule } from '../components/navbar/navbar.module';
+import { ImageListModule } from '../components/image-list/image-list.module';
 
 
 @NgModule({
@@ -14,6 +15,7 @@ import { NavbarModule } from '../components/navbar/navbar.module';
     CommonModule,
     HomePageRoutingModule,
     NavbarModule,
+    ImageListModule,
   ]
 })
 export class HomePageModule { }
