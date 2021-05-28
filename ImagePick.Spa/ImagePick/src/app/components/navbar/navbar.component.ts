48912b2bcb1a13cc54a9bdeaf22326595fa89430
JorgeBritezG <40007364+JorgeBritezG@users.noreply.github.com>
2021-05-28 03:04:50 -0400

    profile-page

--- ImagePick.Spa/ImagePick/src/app/components/navbar/navbar.component.ts
+++ ImagePick.Spa/ImagePick/src/app/components/navbar/navbar.component.ts
@@ -1,15 +1,48 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { SocialAuthService, SocialUser } from 'angularx-social-login';
+import { Observable, Subscription } from 'rxjs';
+import { AuthenticateService } from 'src/app/providers/authenticate.service';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
-export class NavbarComponent implements OnInit {
+export class NavbarComponent implements OnInit, OnDestroy {
+  
+  logged = false;
+  model: SocialUser | null | undefined;
+  subscription: Subscription = new Subscription;
+  authState$: Observable<SocialUser> | undefined;
+  
+  constructor(
+    private socialAuthService: SocialAuthService,
+    private authService: AuthenticateService
 
-  constructor() { }
+    ) { }
 
   ngOnInit(): void {
+    this.authState$ = this.socialAuthService.authState;
+    this.subscription = this.socialAuthService.authState.subscribe(data => {
+      console.log(data);
+      if (data) {
+        this.model = data;
+        this.logged = true;
+      }
+      else {
+        this.model = null;
+        this.logged = false;
+      }
+    });
+
+  }
+  
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
+  }
+
+  logout() {
+    this.authService.logout();
   }
 
 }
--- ImagePick.Spa/ImagePick/src/app/login-page/login-page.component.ts
+++ ImagePick.Spa/ImagePick/src/app/login-page/login-page.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit } from '@angular/core';
-import { GoogleLoginProvider } from "angularx-social-login";
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { GoogleLoginProvider, SocialUser } from "angularx-social-login";
 import { SocialAuthService } from "angularx-social-login";
+import { Observable, Subscription } from 'rxjs';
 import { AuthenticateService } from '../providers/authenticate.service';
 
 @Component({
@@ -8,26 +10,46 @@ import { AuthenticateService } from '../providers/authenticate.service';
   templateUrl: './login-page.component.html',
   styleUrls: ['./login-page.component.css']
 })
-export class LoginPageComponent implements OnInit {
+export class LoginPageComponent implements OnInit, OnDestroy {
 
   isLogging: boolean = false;
 
-  constructor(private authService: AuthenticateService,
+
+  subscription: Subscription = new Subscription;
+
+  constructor(
+    private router: Router,
+    private authService: AuthenticateService,
     private socialLoginService: SocialAuthService) { }
 
 
   ngOnInit(): void {
+
+
+    this.subscription = this.socialLoginService.authState.subscribe(data => {
+      console.log(data);
+      if (data) {
+        this.router.navigate(['/']);
+      }
+    });
+    
   }
 
   signInWithGoogle(): void {
     this.isLogging = true;
     this.socialLoginService.signIn(GoogleLoginProvider.PROVIDER_ID).then(googleUser => {
+      console.log(googleUser);
       this.authService.googleLogin(googleUser)
         .subscribe((data) => {
           console.log(data);
+          this.router.navigate(['/']);
           this.isLogging = false;
         });
     });
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
   }
 
 }
