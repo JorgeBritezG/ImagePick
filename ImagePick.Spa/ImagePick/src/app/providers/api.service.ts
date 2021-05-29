20ce7015006d9975b34b71347f125c1511ca1b04
JorgeBritezG <40007364+JorgeBritezG@users.noreply.github.com>
2021-05-29 00:45:03 -0400

    apiService methods

--- ImagePick.Spa/ImagePick/src/app/providers/api.service.ts
+++ ImagePick.Spa/ImagePick/src/app/providers/api.service.ts
@@ -1,9 +1,37 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
 
-  constructor() { }
+  private apiUrl = environment.baseUrl
+
+  constructor(
+    private http: HttpClient,
+  ) { }
+
+  getAll(controller: string): Observable<any[]> {
+    return this.http.get<any[]>(`${this.apiUrl}/${controller}`);
+  }
+
+  getById(controller: string, id: string): Observable<any[]> {
+    return this.http.get<any[]>(`${this.apiUrl}/${controller}/${id}`);
+  }
+
+  create(model: any, controller: string): Observable<any> {
+    return this.http.post<any>(`${this.apiUrl}/${controller}`, model);
+  }
+
+  update(model: any, controller: string): Observable<any> {
+    return this.http.put<any>(`${this.apiUrl}/${controller}`, model);
+  }
+
+  delete(controller: string, id: string): Observable<any[]> {
+    return this.http.delete<any[]>(`${this.apiUrl}/${controller}/${id}`);
+  }
+
 }
